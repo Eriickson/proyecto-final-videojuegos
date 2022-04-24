fe48880e990ca1d3ef24e55fb1d3aa53a5e1ade4
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-24 08:07:40 -0400

    commit

--- Assets/2. scripts/PlayerController.cs
+++ Assets/2. scripts/PlayerController.cs
@@ -5,29 +5,70 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    public float speed, jumpHeight;
     float speedX, speedY;
     Rigidbody2D rb;
+    public Transform groundCheck;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        FlipCharacter();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+
+
+    public void Movement()
+    {
+
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = rb.velocity.y;
 
         rb.velocity = new Vector2(speedX * speed, speedY);
 
+        if(rb.velocity.x != 0)
+        {
+            anim.SetBool("Fly", true);
+        } else
+        {
+            anim.SetBool("Fly", false);
+        }
+    }
 
-        if(rb.velocity.x > 0)
+    public void Jump()
+    {
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+
+    public void FlipCharacter()
+    {
+        if (rb.velocity.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
-        } else
+        }
+        else
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
